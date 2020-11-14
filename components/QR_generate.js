034d595674413dbb5b67838c2c0a35eaf1a41a64
Jungi Park <byjungy93@gmail.com>
2020-11-14 13:01:02 -0500

    fixed a hook bug on QR component

--- components/QR_generate.js
+++ components/QR_generate.js
@@ -1,61 +1,58 @@
-import React from 'react';
-import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
-import {QRCode} from 'react-native-custom-qr-codes';
-import CountDown from 'react-native-countdown-component';
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useState } from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+import { QRCode } from "react-native-custom-qr-codes";
+import CountDown from "react-native-countdown-component";
 
-export default class GenerateQR extends React.Component{
-  constructor(){
-    super();
-    this.state = {
-      QR : <QRCode style={styles.buttonBsar_itme} content={Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)}/>,
-      ID : "0",
+export default function GenerateQR_func({navigation}) {
+  const [state, setState] = useState( { 
+    QR : <QRCode style={styles.buttonBsar_itme} content={ Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)}/>,
+    ID : "0",
     }
-  };  
+    );
 
- generateQR = () => {
- this.setState({
- QR : <QRCode style={styles.buttonBsar_itme} content={Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)}/>,
- ID : (parseInt(this.state.ID) + 1).toString(),
-  });
- }
+  const generateQR = () => {
+      setState({
+      QR : <QRCode style={styles.buttonBsar_itme} content={Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)}/>,
+      ID : (parseInt(state.ID) + 1).toString(),
+       });
+      }
 
- callBackMain = () => {
-  alert("hello"); 
- }
+  const navigatior = () => navigation.naviator('Home')
 
 
-render(){
-  return(
+  return (
     <View style={styles.buttonBar_itme}>
-      {this.state.QR}
-      {console.log(this.state.ID)}
-      {/* {this.state.ID == 5 && alber} */}
-      <CountDown style={styles.botton_timer}
+      {state.QR}
+      {console.log(state.ID)}
+      {/* {state.ID == 5 && () => navigation.naviator('Home')} */}
+      <CountDown
+        style={styles.botton_timer}
         size={30}
-        id= {this.state.ID}
-        until= {2}
-        onFinish={this.generateQR}
-        digitStyle={{backgroundColor: '#FFF', borderWidth: 2, borderColor: '#1CC625'}}
-        digitTxtStyle={{color: '#1CC625'}}
-        timeLabelStyle={{color: 'red', fontWeight: 'bold'}}
-        separatorStyle={{color: '#1CC625'}}
-        timeToShow={[ 'M', 'S']}
-        timeLabels={{m: null, s: null}}
+        id={state.ID}
+        until={2}
+        onFinish={generateQR}
+        digitStyle={{
+          backgroundColor: "#FFF",
+          borderWidth: 2,
+          borderColor: "#1CC625",
+        }}
+        digitTxtStyle={{ color: "#1CC625" }}
+        timeLabelStyle={{ color: "red", fontWeight: "bold" }}
+        separatorStyle={{ color: "#1CC625" }}
+        timeToShow={["M", "S"]}
+        timeLabels={{ m: null, s: null }}
         showSeparator
       />
     </View>
   );
 }
-}
-
 
 const styles = StyleSheet.create({
   buttonBar_itme: {
-    justifyContent: 'center', 
-    fontWeight: 'bold',
-    alignItems: 'center',
-    flex: 1, 
+    justifyContent: "center",
+    fontWeight: "bold",
+    alignItems: "center",
+    flex: 1,
   },
 
   botton_timer: {
@@ -63,10 +60,10 @@ const styles = StyleSheet.create({
   },
 
   button1: {
-    textAlign: 'center',
-    textAlignVertical: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
+    textAlign: "center",
+    textAlignVertical: "center",
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: "yellow",
     padding: 20,
     fontSize: 20,
@@ -74,5 +71,5 @@ const styles = StyleSheet.create({
     width: 300,
     height: 50,
     top: 25,
-  }
+  },
 });
