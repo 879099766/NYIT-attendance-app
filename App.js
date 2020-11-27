50e9dd521e315bc3ed9a5e077d1fab218d050855
jseo2147 <jseo03@nyit.edu>
2020-11-27 14:16:39 -0500

    temporarily fixed

--- App.js
+++ App.js
@@ -1,8 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Network from 'expo-network';
+import NetInfo from "@react-native-community/netinfo";
+import * as Device from 'expo-device';
 
 export default function App() {
+  
+  NetInfo.fetch().then(state => {
+    console.log("Connection type", state.type);
+    console.log("Is connected?", state.isConnected);
+    if (state.type == "wifi"){
+
+      console.log("Device is connected to WiFi!");
+      let OS = Device.osName;
+      console.log("OS of device: ", OS);
+      let brand = Device.brand;
+      let model_name = Device.modelName;
+      console.log("Device brand: ", brand);
+      console.log("Model name: ", model_name);
+      let i_p = state.details.ipAddress;
+      console.log("IP Address:", i_p);
+      let s_sid= state.details.ssid;
+      console.log("SSID:", s_sid);
+      let mac = Network.getMacAddressAsync({});
+      console.log("MAC: ", mac);
+    }
+    else
+      console.log("Device is not connected to WiFi.");
+  });
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
