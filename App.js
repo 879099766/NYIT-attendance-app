74dcc846b4e2151ec4af6d95ab56c475a3eba24b
juliacapuana <juliacapuana@gmail.com>
2020-12-08 23:59:34 -0500

    First commit

--- App.js
+++ App.js
@@ -3,12 +3,38 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //As January is 0.
+var yyyy = today.getFullYear();
+  
+if(mm>=1 && mm<=5)
+{
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>It is the Spring Semester.</Text>
       <StatusBar style="auto" />
     </View>
   );
+}
+else if(mm>=9 && mm<=12)
+{
+  return (
+    <View style={styles.container}>
+  <Text>It is the Fall Semester.</Text>
+  <StatusBar style="auto" />
+</View>
+  )
+}
+else
+{
+return(
+  <View style={styles.container}>
+  <Text>It is the summer.</Text>
+  <StatusBar style="auto" />
+</View>
+)
+}
 }
 
 const styles = StyleSheet.create({
@@ -19,3 +45,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+
